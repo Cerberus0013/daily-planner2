f3b9db06394e8e92e1b575c714974cc6224c2ec8
Breedlove13 <b.breedlove@outlook.com>
2021-04-04 13:01:51 -0700

    basic bootrstap added

---
+++ index.js
@@ -0,0 +1,10 @@
+//*need to input current date into top part using moment
+
+
+//*on click the boxes open up for input
+
+//* create a save function to save whats been put in
+
+//*color red for past to-do stuff
+
+//*green for upcoming.
