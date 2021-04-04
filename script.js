62ce56f12f095e94aa62d3275008e01cc96cf38b
Breedlove13 <b.breedlove@outlook.com>
2021-04-04 14:31:22 -0700

    current date added

--- script.js
+++ script.js
@@ -9,4 +9,33 @@
 
 //*green for upcoming.
 
-$(#currentDay).moment().format("MMM DO YY")
+let today = moment();
+$('#currentDay').append("Today is " + today.format('dddd Do MMMM, YYYY'))
+
+
+
+$(".editable").on("click", function () {
+  var text = $(this).text().trim()
+
+  var textInput = $("<teaxtarea>").val(text);
+
+  $(this).replaceWith(textInput);
+
+  textInput.trigger("focus");
+});
+
+$(".editable").on("blur", "textarea", function () {
+var text = $(this).val().trim();
+
+
+var status = $(this).closest(".list-group").attr("id").replace("list-", "");
+
+
+var index = $(this).closest(".list-group-item").index();
+
+});
+
+
+
+$('.saveble')
+
